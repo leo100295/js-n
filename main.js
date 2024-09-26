48f854a22cf5a2704a0f4f454c0f51b4c010624f
leo100295 <leonardo.macedo10@escola.pr.gov.br>
2024-09-26 12:26:12 -0300

    t

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ return lado**2;
 console.log(areaQuadrado(6));
 
 function vdd(){
-    let espe = prompt("Coloque o numero:");
-    let par = parseInt(espe);
-    let resultado = par / 2;
+    let espe = prompt("Coloque o primeiro numero:");
+    let espe2 = prompt("Coloque o segundo numero: ")
+    let espe3 = prompt("Coloque o terceiro numero: ")
+    let resultado = espe + espe2 + espe3 / 2;
     return resultado;
 }
 console.log(vdd());
