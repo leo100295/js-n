1513770c678193a13370cc4a61591224f3d9b72b
leo100295 <leonardo.macedo10@escola.pr.gov.br>
2024-09-26 11:41:39 -0300

    vgj

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function soma(num1, num2){
 }
 console.log(soma(1,2));
 
-function area(num, num2){
-    return num*num2
+function area(lado){
+    return lado**2
 }
-console.log(area(1,2));
+console.log(areaQuadrado(6)));
