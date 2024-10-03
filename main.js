3bb0eff21cc0d66ac186fabcb73d49a46cb1c35d
leo100295 <leonardo.macedo10@escola.pr.gov.br>
2024-10-03 12:08:45 -0300

    sg

--- main.js
+++ main.js
@@ -19,7 +19,7 @@
 // console.log(vdd());
  let estados=['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
 let i =0;
- while(i<27){
+ while(i<estados.length){
  console.log(i);
  console.log(estados[i]);
 
