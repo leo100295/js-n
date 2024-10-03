21fb7b42129e30fa46f9e494abce54b8500ec109
leo100295 <leonardo.macedo10@escola.pr.gov.br>
2024-10-03 11:22:13 -0300

    E

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-alert("Alice Miranda & Leonardo Macedo")
-function soma(num1, num2){
-    return num1+num2
-}
-console.log(soma(1,2));
+// alert("Alice Miranda & Leonardo Macedo")
+// function soma(num1, num2){
+//     return num1+num2
+// }
+// console.log(soma(1,2));
 
-function areaQuadrado(lado){
-return lado**2;
-}
-console.log(areaQuadrado(6));
+// function areaQuadrado(lado){
+// return lado**2;
+// }
+// console.log(areaQuadrado(6));
 
-function vdd(){
-    let espe = prompt("Coloque o primeiro numero:");
-    let espe2 = prompt("Coloque o segundo numero: ")
-    let espe3 = prompt("Coloque o terceiro numero: ")
-    let resultado = espe + espe2 + espe3 / 3;
-    return resultado;
-}
-console.log(vdd());
+// function vdd(){
+//     let espe = prompt("Coloque o primeiro numero:");
+//     let espe2 = prompt("Coloque o segundo numero: ")
+//     let espe3 = prompt("Coloque o terceiro numero: ")
+//     let resultado = espe + espe2 + espe3 / 3;
+//     return resultado;
+// }
+// console.log(vdd());
+ let estados=['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
